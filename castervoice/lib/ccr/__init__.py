a6136b3379669d945378bd4c28040e3e8430c8f9
Alex Boche <akboche@gmail.com>
2019-03-06 19:44:27 -0800

    add dragonfly dev commands and make it a ccr rule within caster

--- castervoice/lib/ccr/__init__.py
+++ castervoice/lib/ccr/__init__.py
@@ -24,6 +24,7 @@ command_sets = {
     "sql.sql": ("SQL", ),
     "prolog.prolog": ("Prolog", ),
     "vhdl.vhdl": ("VHDL", ),
+    "dragonfly_dev_commands.dragonfly_dev_commands": ("Dragonflydev", ),
 }
 
 for module_name, class_name_tup in command_sets.iteritems():

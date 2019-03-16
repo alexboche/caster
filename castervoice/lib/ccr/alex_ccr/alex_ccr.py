98318f38d46f53b6d6393b12407da7b7e16956b0
Alex Boche <akboche@gmail.com>
2019-03-16 13:21:35 -0700

    add squat to alex_ccr

--- castervoice/lib/ccr/alex_ccr/alex_ccr.py
+++ castervoice/lib/ccr/alex_ccr/alex_ccr.py
@@ -10,70 +10,8 @@ from castervoice.apps import utils
 
 from dragonfly.actions.action_mouse import get_cursor_position
 
-
-def split_dictation(text):
-    if text:
-        words = ['"%s"' % word for word in text.format().split()]
-        words = ', '.join(words)
-    else:
-        words = '""'
-
-    return words
-
-
-def type_split_dictation(text):
-    Text(split_dictation(text)).execute()
-
-
-def type_mimic(text):
- 
-   # Create an argument list for Mimic from text.
-    words = split_dictation(text)
-
-    # Define the action to execute. Move back only if no words were
-    # specified.
-    action = Text('Mimic(%s)' % words)
-    if not text:
-        action += Key("left:2")
-
-    # Execute the action.
-    action.execute()
-
-
-def type_playback(text):
-    # Create an argument list for Playback from text.
-    words = split_dictation(text)
-
-    # Define the action to execute.
-    # This will press enter a few times. Indentation will depend on your
-    # editor.
-    enter = Key("enter")
-    action = (Text('Playback([') + enter +
-              Text('([%s], 0.0),' % words) + enter +
-              Text('])') + Key("up, end"))
-
-    # Move back only if no words were specified.
-    if not text:
-        action += Key("left:9")
-
-    # Execute the action.
-    action.execute()
-
-
-def type_mouse(mouse_button):
-    action = Text('Mouse("%s")' % mouse_button)
-
-    # Move back only if no words were specified.
-    if not mouse_button:
-        action += Key("left:2")
-
-    # Execute the action.
-    action.execute()
-
-def type_mouse_current():
-    # Type Mouse("[X, Y]"). The typed Mouse action will move the cursor to
-    # back to where it was when the command was spoken.
-    Text('Mouse("[%d, %d]")' % get_cursor_position()).execute()
+_NEXUS = control.nexus()
+    
 
 
 class AlexCcrRule(MergeRule):
@@ -87,7 +25,10 @@ class AlexCcrRule(MergeRule):
         "middle click": Mouse("middle"),
         "say <dict>": Text("%(dict)s"),        
         "center click": Mouse("[0.5, 0.5], left"),
-
+        "squat":
+            R(Function(navigation.left_down, nexus=_NEXUS), rdescript="Mouse: Left Down"),
+        "bench":
+            R(Function(navigation.left_up, nexus=_NEXUS), rdescript="Mouse: Left Up"),
 
         "hard delete [<n>]": Key("s-delete") * Repeat(extra='n'),
         "lanter": Key("home"),

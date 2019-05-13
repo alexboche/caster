d82af28d5d0354acf5e0baf3ce0e01e506f65edb
Alex Boche <akboche@gmail.com>
2019-05-13 12:29:48 -0700

    Sunday night regex word boundary working

--- castervoice/lib/navigation.py
+++ castervoice/lib/navigation.py
@@ -7,6 +7,7 @@ import time
 from ctypes import windll
 from subprocess import Popen
 import pyperclip
+import re
 
 import dragonfly
 from dragonfly import Choice, monitors, Pause
@@ -15,6 +16,7 @@ from castervoice.lib import control, settings, utilities, textformat
 from castervoice.lib.actions import Key, Text, Mouse
 from castervoice.lib.clipboard import Clipboard
 from castervoice.lib import settings, context
+
 
 
 DIRECTION_STANDARD = {
@@ -59,24 +61,53 @@ def dragon_capitalize(phrase):
             word_list.pop(i)
     return word_list
 
-def replace_phrase_with_phrase(text, replaced_phrase, replacement_phrase, left_right):
     
+
+punctuation_list = [".", ",", "'", "[", "]", "<", ">", "{", "}", "?", "–", "-", ";", "="]
+
+def replace_phrase_with_phrase(text, replaced_phrase, replacement_phrase, left_right):
     if left_right == "left":
         # for matching purposes use lowercase
-        left_index = (text.lower()).rfind(replaced_phrase.lower())
+        # left_index = (text.lower()).rfind(replaced_phrase.lower())
+        if replaced_phrase in punctuation_list:
+            pattern = re.escape(replaced_phrase)
+        else:
+            pattern = r"\b" + re.escape(replaced_phrase.lower()) + r"\b"            
+        
+        if not re.search(pattern, text.lower()):
+            # replaced phase not found
+            print("'{}' not found".format(replaced_phrase))
+            return
+        
+        match_iter = re.finditer(pattern, text.lower())
+        match_list = [(m.start(), m.end()) for m in match_iter]
+        last_match = match_list[-1]
+        left_index, right_index = last_match
+
+
     if left_right == "right":
-        left_index = (text.lower()).find(replaced_phrase.lower())
-    # the if statement below is independent of whether left_right equals left or right
-    if left_index == -1:
-        print("'{}' not found".format(replaced_phrase))
-        return
-    right_index = left_index + len(replaced_phrase)
+        # left_index = (text.lower()).find(replaced_phrase.lower())
+        # if replaced phrase is punctuation, don't require a word boundary for match
+        if replaced_phrase in punctuation_list:
+            pattern = re.escape(replaced_phrase.lower())
+        # replaced_phrase contains a word
+        else:
+            pattern = r"\b" + re.escape(replaced_phrase.lower()) + r"\b"
+        match = re.search(pattern, text.lower())
+        if not match:
+            print("'{}' not found".format(replaced_phrase))
+            return
+        else:
+            left_index, right_index = match.span()
+            print("left_index: {}, right_index: {}").format(left_index, right_index)
+    # right_index = left_index + len(replaced_phrase)
     return text[: left_index] + replacement_phrase + text[right_index:] 
     
 
 # comments show how to do it using pyperclip instead of caster's method
     # unlike pyperclip caster's method does not alter the clipboard 
 def copypaste_replace_phrase_with_phrase(replaced_phrase, replacement_phrase, left_right):
+    # temporarily store previous clipboard item
     temp_for_previous_clipboard_item = pyperclip.paste()
     Pause("30").execute()
 
@@ -95,8 +126,14 @@ def copypaste_replace_phrase_with_phrase(replaced_phrase, replacement_phrase, le
     
     replaced_phrase = str(replaced_phrase)
     replacement_phrase = str(replacement_phrase)
-    # replacement_phrase_with_capitals = dragone_capitalize(replacement_phrase)
     new_text = replace_phrase_with_phrase(selected_text, replaced_phrase, replacement_phrase, left_right)
+    if not new_text:
+        # text not found
+        Key("c-v").execute()
+        if left_right == "right":
+            print("right")
+            Key("left:%d" %len(selected_text)).execute()
+        return
     # if not context.paste_string_without_altering_clipboard(new_text):
     #     print("failed to paste {}".format(new_text))
     pyperclip.copy(new_text)
@@ -119,7 +156,6 @@ def remove_phrase_from_text(text, phrase, left_right):
         return 
     right_index = left_index + len(phrase)
     # if the "phrase" is punctuation, just remove it, but otherwise remove an extra space adjacent to the phrase
-    punctuation_list = [".", ",", "'", "[", "]", "<", ">", "{", "}", "?", "–", "-", ";", "="]
     if phrase in punctuation_list:
         return text[: left_index] + text[right_index:] 
     else:
@@ -127,6 +163,7 @@ def remove_phrase_from_text(text, phrase, left_right):
 
 
 def copypaste_remove_phrase_from_text(phrase, left_right):
+    # temporarily store previous clipboard item
     temp_for_previous_clipboard_item = pyperclip.paste()
     Pause("30").execute()
 
@@ -148,48 +185,64 @@ def copypaste_remove_phrase_from_text(phrase, left_right):
 
     phrase = str(phrase)
     new_text = remove_phrase_from_text(selected_text, phrase, left_right)
-    if not context.paste_string_without_altering_clipboard(new_text):
-        print("failed to paste {}".format(new_text))
-        return
+    # if not context.paste_string_without_altering_clipboard(new_text):
+    #     print("failed to paste {}".format(new_text))
+    #     return
+    pyperclip.copy(new_text)
+    Key("c-v").execute()
 
     if left_right == "right":
         offset = len(new_text)
         Key("left:%d" %offset).execute()
+    # put previous clipboard item back in the clipboard
+    Pause("20").execute()
+    pyperclip.copy(temp_for_previous_clipboard_item)
 
 
 def move_until_character_sequence(left_right, character_sequence):
+    # temporarily store previous clipboard item
+    # temp_for_previous_clipboard_item = pyperclip.paste()
+
     if left_right == "left":
-        Key("s-home").execute()
+        Key("s-home, c-c/2").execute()
     if left_right == "right":
-        Key("s-end").execute()
-    err, selected_text = context.read_selected_without_altering_clipboard()
-    if err != 0:
+        Key("s-end, c-c/2").execute()
+    Pause("50").execute()
+    # err, selected_text = context.read_selected_without_altering_clipboard()
+    # if err != 0:
         # I'm not discriminating between err = 1 and err = 2
-        print("failed to copy text")
-        return
-    # Pause("100").execute()
-    text = str(selected_text).lower()
-        # don't distinguish between upper and lowercase
+        # print("failed to copy text")
+        # return
+    selected_text = pyperclip.paste()
+    Pause("50").execute()
+    selected_text = str(selected_text).lower()
+    
+        # don't distinguish between upper and lowercase for purposes of search
     character_sequence = str(character_sequence).lower()
     if left_right == "left":
-        Key("right").execute() # unselect text
-        if text.rfind(character_sequence) == -1:
+        # Key("right").execute() # unselect text
+        if selected_text.rfind(character_sequence) == -1:
             print("'{}' not found".format(character_sequence))
             return 
         else:
-            character_sequence_start_position = text.rfind(character_sequence) + len(character_sequence)
-            offset = len(text) - character_sequence_start_position 
+            Key("c-v/20").execute()
+            character_sequence_start_position = selected_text.rfind(character_sequence) + len(character_sequence)
+            offset = len(selected_text) - character_sequence_start_position 
             Key("left:%d" %offset).execute()
     if left_right == "right":
         Key("left").execute() # unselect text
-        if text.find(character_sequence) == -1:
+        if selected_text.find(character_sequence) == -1:
             print("'{}' not found".format(character_sequence))
             return 
         else:
-            character_sequence_start_position = text.find(character_sequence) 
+            character_sequence_start_position = selected_text.find(character_sequence) 
             offset = character_sequence_start_position 
             Key("right:%d" %offset).execute()
-        
+    
+    # put previous clipboard item back in the clipboard
+    # Pause("20").execute()
+    # pyperclip.copy(temp_for_previous_clipboard_item)
+
 
 
 def delete_until_character_sequence(text, character_sequence, left_right):
